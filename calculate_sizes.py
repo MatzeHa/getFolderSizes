b3ba1022e3ccdb22a1ebef31b23cd8aa8bf7a903
MatzeHa <m.hainmueller@gmail.com>
2023-03-11 13:29:07 +0100

    - sorting sizes works

--- calculate_sizes.py
+++ calculate_sizes.py
@@ -3,23 +3,20 @@ import os
 
 def calculate_sizes(app):
     folders = []
+    result_list = []
     app_root = app.path.get()
     app.write_to_txt_results(app_root)
     for _dir in os.listdir(app_root):
         folders.append(os.path.join(app_root, _dir))
-    print(folders)
-
-    print("Start Counting")
     for folder in folders:
-        print("FOLDER")
-        print(folder)
         dir_size = 0
         for root, dirs, files in os.walk(folder, topdown=True):
-            print(root)
             for file in files:
                 f_path = os.path.join(root, file)
                 file_size = os.stat(f_path).st_size
                 dir_size += file_size
-        print(dir_size)
         dir_size_mb = dir_size / (1024*1024)
-        app.write_to_txt_results(folder + " ---> " + str(dir_size_mb))
+        result_list.append([folder, dir_size_mb])
+    return result_list
+
+
--- main.py
+++ main.py
@@ -23,6 +23,7 @@ class MainApp:
 
         # create widgets
         self.widgets = {}
+        self.result_widgets = []
 
         btn_choose_dir = tk.Button(self.window, text="Wähle Verzeichnis", command=self.choose_directory)
         btn_choose_dir.grid(row=0, column=1)
@@ -31,11 +32,11 @@ class MainApp:
         lbl_path.grid(row=0, column=2, columnspan=3)
         self.widgets["LBL_PATH"] = lbl_path
 
-        txt_results = tk.Text(self.window, width=100, height=30, state="disabled")
+        txt_results = tk.Text(self.window, width=80, height=2, state="disabled")
         scroll_results = ttk.Scrollbar(self.window, command=txt_results.yview)
         txt_results.configure(yscrollcommand=scroll_results.set)
-        txt_results.grid(row=1, column=1, rowspan=3, columnspan=4)
-        scroll_results.grid(row=1, column=4, rowspan=3, sticky="NSE")
+        txt_results.grid(row=1, column=1, rowspan=1, columnspan=4)
+        scroll_results.grid(row=1, column=4, rowspan=1, sticky="NSE")
         self.widgets["TXT_RESULTS"] = txt_results
         self.widgets["SCROLL_RESULTS"] = scroll_results
 
@@ -65,7 +66,42 @@ class MainApp:
         self.window.destroy()
 
     def start_calculation(self):
-        calculate_sizes(self)
+        dir_sizes = calculate_sizes(self)
+        dir_sizes = self.sort_sizes(dir_sizes)
+        self.create_result_table(dir_sizes)
+
+    def sort_sizes(self, dir_sizes):
+
+        for i in range(len(dir_sizes)):
+            print(i)
+        dir_sizes.sort(key=lambda a: a[1], reverse=True)
+
+        return dir_sizes
+
+    def create_result_table(self, dir_sizes):
+        start_row = 3
+        if len(dir_sizes) == 0:
+            self.write_to_txt_results("Keine Unterordner...")
+            return
+        total_rows = len(dir_sizes)
+        print(dir_sizes)
+        for i in range(total_rows):
+            print(i)
+            new_cell = tk.Entry(self.window, width=100)
+            new_cell.grid(row=start_row+i, column=1)
+            new_cell.config(state="normal")
+            new_cell.insert(tk.END, str(dir_sizes[i][0]))
+            new_cell.config(state="disabled")
+            self.result_widgets.append(new_cell)
+            new_cell = tk.Entry(self.window, width=8)
+            new_cell.grid(row=start_row+i, column=2)
+            new_cell.config(state="normal")
+            new_cell.insert(tk.END, str(dir_sizes[i][1]))
+            new_cell.config(state="disabled")
+            self.result_widgets.append(new_cell)
+
+        self.widgets["BTN_START_CALCULATION"].grid(row=start_row+i+2)
+        self.widgets["BTN_EXIT"].grid(row=start_row+i+2)
 
 
 if __name__ == "__main__":
